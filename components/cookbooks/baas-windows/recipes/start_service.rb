3e5296d366d6024ade19b719b9f918e3f393844e
nithya.swaminathan@walmart.com <nithya.swaminathan@walmart.com>
2018-05-08 14:42:06 +0530

    fixed errors in start and stop service

--- components/cookbooks/baas-windows/recipes/start_service.rb
+++ components/cookbooks/baas-windows/recipes/start_service.rb
@@ -1,4 +1,4 @@
-if node[:workorder][:services].has_key?('dotnet-platform') && node[:workorder][:services]["dotnet-platform"][cloud]['ciAttributes']['baas_package_details']).has_key?('baasdriver')
+if (node[:workorder][:services].has_key?('dotnet-platform') && node[:workorder][:services]["dotnet-platform"][cloud]['ciAttributes']['baas_package_details'].has_key?('baasdriver'))
         windows_service_name = "baasdriver"
 
         windowsservice windows_service_name do
--- components/cookbooks/baas-windows/recipes/stop_service.rb
+++ components/cookbooks/baas-windows/recipes/stop_service.rb
@@ -1,5 +1,6 @@
+ cloud = node.workorder.cloud.ciName
 if node[:workorder][:services].has_key?('dotnet-platform')
-    if node[:workorder][:services]["dotnet-platform"][cloud]['ciAttributes']['baas_package_details']).has_key?('baasdriver')
+    if node[:workorder][:services]["dotnet-platform"][cloud]['ciAttributes']['baas_package_details'].has_key?('baasdriver')
         windows_service_name = "baasdriver"
 
         windowsservice windows_service_name do
